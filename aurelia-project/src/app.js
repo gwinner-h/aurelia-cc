5fb2cc456b49732c946fe2772c9c67bfd4b5dfff
Heidi Gwinner <heidi.gwinner@weareideal.com>
2026-04-02 13:43:03 -0400

    corrected constructor

--- aurelia-project/src/app.js
+++ aurelia-project/src/app.js
@@ -1,6 +1,9 @@
 import { Todo } from './todo';
 
 export class App {
-  message = 'Hello World!';
-  this.todo = new Todo('Learn Aurelia');
+  constructor() {
+    this.message = 'Hello World!';
+    this.todo = new Todo('Learn Aurelia');
+    this.todo.done = true
+  }
 }

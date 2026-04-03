1b56ad4e6af221fb451c4045778e161efac2a4de
heidi gwinner <heidi.gwinner@weareideal.com>
2026-04-03 13:02:16 -0400

    added files

--- aurelia-project/src/app.js
+++ aurelia-project/src/app.js
@@ -8,9 +8,9 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Router Test';
     config.map([
-      {route: '', name: 'home', moduleId: PLATFORM.moduleName('index'), title: 'home'},
-      {route: 'about', name: 'about', moduleId: PLATFORM.moduleName('about'), title: 'About'},
-      {route: 'post', name: 'post', moduleId: PLATFORM.moduleName('post'), title: 'post'}
+      {route: '',       name: 'home',   moduleId: PLATFORM.moduleName('index'), title: 'home'},
+      {route: 'about',  name: 'about',  moduleId: PLATFORM.moduleName('about'), title: 'About'},
+      {route: 'post',   name: 'post',   moduleId: PLATFORM.moduleName('post'),  title: 'post'}
     ]);
   }
 
